d0e364940a95deb64dbf27b8051620421d9fe6d3
davood96 <david96@my.yorku.ca>
2017-09-08 15:58:58 -0400

    Fixed bug where calculator was unresponsive after exiting modal

--- Javascript/Calculator/calc_controller.js
+++ Javascript/Calculator/calc_controller.js
@@ -5,8 +5,6 @@
  */
 
 var calc_model = null;
-var history_field;
-var value_field;
 
 function init()
 {
@@ -26,6 +24,8 @@ function registerKey(key)
     {
         init();
     }
+    var history_field = window.document.getElementById("history");
+    var value_field = window.document.getElementById("main");
     var result = calc_model.registerModel(key, history_field.innerHTML, value_field.innerHTML);
     value_field.innerHTML = result[0];
     history_field.innerHTML = result[1];
@@ -35,6 +35,8 @@ function undoKey()
 {
     if(!calc_model.isInitial())
     {
+         var history_field = window.document.getElementById("history");
+         var value_field = window.document.getElementById("main");
          var result = calc_model.undoModel();
          value_field.innerHTML = result[0];
          history_field.innerHTML = result[1];
